4f1795537d86a5af9fe035c06297651c636d306d
fuqirui <846639569@qq.com>
2024-01-04 01:18:10 +0800

    rewrite intercode.h/c

--- intercode.c
+++ intercode.c
@@ -1,183 +1,188 @@
-// #include "intercode.h"
-// #include<assert.h>
+#include "intercode.h"
+#include<assert.h>
 
-// void Add_InterCode(InterCodeNode* head, InterCode code){
-//     InterCodeNode *cur = head;
-//     InterCodeNode *p = (InterCodeNode*)malloc(sizeof(InterCodeNode));
-//     p->node = code;
-//     p->next = NULL;
+void Print_OP(Operand op, FILE *f){
+    switch(op.kind){
+        case OP_VARI:
+            fprintf(f, "variable%d", op.u.no_vari);
+            break;
+        case OP_FUNC:
+            fprintf(f, "%s", op.u.func_name);
+            break;
+        case OP_LABEL:
+            fprintf(f, "label%d", op.u.no_label);
+            break;
+        case OP_CONST:
+            fprintf(f, "#%d", op.u.val_const);
+            break;
+        default:
+            assert(0);
+    }
+}
 
-//     while(cur->next!=NULL) cur = cur->next;
+void PrintCode(InterCode code, FILE* f){
+    switch(code.kind){
+        case LABEL:
+            fprintf(f, "LABEL ");
+            Print_OP(code.u.op1, f);
+            fprintf(f, " :\n");
+            break;
+        case FUNC:
+            fprintf(f, "FUNCTION ");
+            Print_OP(code.u.op1, f);
+            fprintf(f, " :\n");
+            break;
+        case ASSIGN_VAL2VAL:
+            Print_OP(code.u.op1, f);
+            fprintf(f, " := ");
+            Print_OP(code.u.op2, f);
+            fprintf(f, "\n");
+            break;
+        case ADD:
+            Print_OP(code.u.op1, f);
+            fprintf(f, " := ");
+            Print_OP(code.u.op2, f);
+            fprintf(f, " + ");
+            Print_OP(code.u.op3, f);
+            fprintf(f, "\n");
+            break;
+        case SUB:
+            Print_OP(code.u.op1, f);
+            fprintf(f, " := ");
+            Print_OP(code.u.op2, f);
+            fprintf(f, " - ");
+            Print_OP(code.u.op3, f);
+            fprintf(f, "\n");
+            break;
+        case MULTI:
+            Print_OP(code.u.op1, f);
+            fprintf(f, " := ");
+            Print_OP(code.u.op2, f);
+            fprintf(f, " * ");
+            Print_OP(code.u.op3, f);
+            fprintf(f, "\n");
+            break;
+        case DIV:
+            Print_OP(code.u.op1, f);
+            fprintf(f, " := ");
+            Print_OP(code.u.op2, f);
+            fprintf(f, " / ");
+            Print_OP(code.u.op3, f);
+            fprintf(f, "\n");
+            break;
+        case ASSIGN_ADD2VAL:
+            Print_OP(code.u.op1, f);
+            fprintf(f, " := &");
+            Print_OP(code.u.op2, f);
+            fprintf(f, "\n");
+            break;
+        case ASSIGN_PNT2VAL:
+            Print_OP(code.u.op1, f);
+            fprintf(f, " := *");
+            Print_OP(code.u.op2, f);
+            fprintf(f, "\n");
+            break;
+        case ASSIGN_VAL2PNT:
+            fprintf(f, "*");
+            Print_OP(code.u.op1, f);
+            fprintf(f, " := ");
+            Print_OP(code.u.op2, f);
+            fprintf(f, "\n");
+            break;
+        case GOTO:
+            fprintf(f, "GOTO ");
+            Print_OP(code.u.op1, f);
+            fprintf(f, "\n");
+            break;
+        case GOTO_COND:
+            fprintf(f, "IF ");
+            Print_OP(code.u.op1, f);
+            fprintf(f, " %s ", code.u.rel);
+            Print_OP(code.u.op2, f);
+            fprintf(f, " GOTO ");
+            Print_OP(code.u.op3, f);
+            fprintf(f, "\n");
+            break;
+        case RETURN:
+            fprintf(f, "RETURN ");
+            Print_OP(code.u.op1, f);
+            fprintf(f, "\n");
+            break;
+        case DEC:
+            fprintf(f, "DEC ");
+            Print_OP(code.u.op1, f);
+            fprintf(f, " %d\n", code.u.op2.u.val_const);
+            break;
+        case ARG:
+            fprintf(f, "ARG ");
+            Print_OP(code.u.op1, f);
+            fprintf(f, "\n");
+            break;
+        case CALL:
+            Print_OP(code.u.op1, f);
+            fprintf(f, " := CALL ");
+            Print_OP(code.u.op2, f);
+            fprintf(f, "\n");
+            break;
+        case PARAM:
+            fprintf(f, "PARAM ");
+            Print_OP(code.u.op1, f);
+            fprintf(f, "\n");
+            break;
+        case READ:
+            fprintf(f, "READ ");
+            Print_OP(code.u.op1, f);
+            fprintf(f, "\n");
+            break;
+        case WRITE:
+            fprintf(f, "WRITE ");
+            Print_OP(code.u.op1, f);
+            fprintf(f, "\n");
+            break;
+        default:
+            assert(0);
+    }
+}
 
-//     cur->next = p;
-// }
+InterCode GenCode(int kind, Operand op1, Operand op2, Operand op3, char rel[]){
+    InterCode res;
+    res.kind = kind;
+    res.u.op1 = op1;
+    res.u.op2 = op2;
+    res.u.op3 = op3;
+    strcpy(res.u.rel, rel);
+    return res;
+}
 
+InterCodeList* CodeListInit(){
+    InterCodeList* self = (InterCodeList*)malloc(sizeof(InterCodeList));
+    self->head_ = NULL;
+    self->tail_ = NULL;
+    return self;
+}
 
-// void Print_OP(Operand op, FILE *f){
-//     switch(op.kind){
-//         case OP_VARI:
-//             fprintf(f, "variable%d", op.u.no_vari);
-//             break;
-//         case OP_FUNC:
-//             fprintf(f, "%s", op.u.name_func);
-//             break;
-//         case OP_ARRAY:
-//             fprintf(f, "&array%d", op.u.no_array);
-//             break;
-//         case OP_LABEL:
-//             fprintf(f, "label%d", op.u.no_label);
-//             break;
-//         case OP_CONST:
-//             fprintf(f, "#%d", op.u.val_const);
-//             break;
-//         case OP_ADD:
-//             fprintf(f, "addr%d", op.u.no_add);
-//             break;
-//         default:
-//             assert(0);
-//     }
-// }
+void AddInterCode(InterCodeList* self, InterCode code){
+    InterCodeNode *cur_tail = self->tail_;
+    InterCodeNode *p = (InterCodeNode*)malloc(sizeof(InterCodeNode));
+    p->node_ = code;
+    p->next = NULL;
 
-// void Print_Code(InterCode code, FILE* f){
-//     switch(code.kind){
-//         case LABEL:
-//             fprintf(f, "LABEL ");
-//             Print_OP(code.u.op1, f);
-//             fprintf(f, " :\n");
-//             break;
-//         case FUNC:
-//             fprintf(f, "FUNCTION ");
-//             Print_OP(code.u.op1, f);
-//             fprintf(f, " :\n");
-//             break;
-//         case ASSIGN_VAL2VAL:
-//             Print_OP(code.u.op1, f);
-//             fprintf(f, " := ");
-//             Print_OP(code.u.op2, f);
-//             fprintf(f, "\n");
-//             break;
-//         case ADD:
-//             Print_OP(code.u.op1, f);
-//             fprintf(f, " := ");
-//             Print_OP(code.u.op2, f);
-//             fprintf(f, " + ");
-//             Print_OP(code.u.op3, f);
-//             fprintf(f, "\n");
-//             break;
-//         case SUB:
-//             Print_OP(code.u.op1, f);
-//             fprintf(f, " := ");
-//             Print_OP(code.u.op2, f);
-//             fprintf(f, " - ");
-//             Print_OP(code.u.op3, f);
-//             fprintf(f, "\n");
-//             break;
-//         case MULTI:
-//             Print_OP(code.u.op1, f);
-//             fprintf(f, " := ");
-//             Print_OP(code.u.op2, f);
-//             fprintf(f, " * ");
-//             Print_OP(code.u.op3, f);
-//             fprintf(f, "\n");
-//             break;
-//         case DIV:
-//             Print_OP(code.u.op1, f);
-//             fprintf(f, " := ");
-//             Print_OP(code.u.op2, f);
-//             fprintf(f, " / ");
-//             Print_OP(code.u.op3, f);
-//             fprintf(f, "\n");
-//             break;
-//         case ASSIGN_ADD2VAL:
-//             Print_OP(code.u.op1, f);
-//             fprintf(f, " := &");
-//             Print_OP(code.u.op2, f);
-//             fprintf(f, "\n");
-//             break;
-//         case ASSIGN_PNT2VAL:
-//             Print_OP(code.u.op1, f);
-//             fprintf(f, " := *");
-//             Print_OP(code.u.op2, f);
-//             fprintf(f, "\n");
-//             break;
-//         case ASSIGN_VAL2PNT:
-//             fprintf(f, "*");
-//             Print_OP(code.u.op1, f);
-//             fprintf(f, " := ");
-//             Print_OP(code.u.op2, f);
-//             fprintf(f, "\n");
-//             break;
-//         case GOTO:
-//             fprintf(f, "GOTO ");
-//             Print_OP(code.u.op1, f);
-//             fprintf(f, "\n");
-//             break;
-//         case GOTO_COND:
-//             fprintf(f, "IF ");
-//             Print_OP(code.u.op1, f);
-//             fprintf(f, " %s ", code.u.rel);
-//             Print_OP(code.u.op2, f);
-//             fprintf(f, " GOTO ");
-//             Print_OP(code.u.op3, f);
-//             fprintf(f, "\n");
-//             break;
-//         case RETURN:
-//             fprintf(f, "RETURN ");
-//             Print_OP(code.u.op1, f);
-//             fprintf(f, "\n");
-//             break;
-//         case DEC:
-//             fprintf(f, "DEC ");
-//             Print_OP(code.u.op1, f);
-//             fprintf(f, " %d\n", code.u.size);
-//             break;
-//         case ARG:
-//             fprintf(f, "ARG ");
-//             Print_OP(code.u.op1, f);
-//             fprintf(f, "\n");
-//             break;
-//         case CALL:
-//             Print_OP(code.u.op1, f);
-//             fprintf(f, " := CALL ");
-//             Print_OP(code.u.op2, f);
-//             fprintf(f, "\n");
-//             break;
-//         case PARAM:
-//             fprintf(f, "PARAM ");
-//             Print_OP(code.u.op1, f);
-//             fprintf(f, "\n");
-//             break;
-//         case READ:
-//             fprintf(f, "READ ");
-//             Print_OP(code.u.op1, f);
-//             fprintf(f, "\n");
-//             break;
-//         case WRITE:
-//             fprintf(f, "WRITE ");
-//             Print_OP(code.u.op1, f);
-//             fprintf(f, "\n");
-//             break;
-//         default:
-//             assert(0);
-//     }
-// }
+    if(cur_tail==NULL){
+        p->prev = NULL;
+        self->head_ = self->tail_ = p;
+    }
+    else{
+        p->prev = cur_tail;
+        cur_tail->next = p;
+        self->tail_ = p;
+    }
+}
 
-// void Print_List(InterCodeNode* head, FILE *f){
-//     InterCodeNode* cur = head->next;
-//     while(cur!=NULL){
-//         Print_Code(cur->node, f);
-//         cur = cur->next;
-//     }
-//     return;
-// }
-
-// InterCode Gen_Code(int kind, Operand op1, Operand op2, Operand op3, int size, char *rel){
-//     InterCode res;
-//     res.kind = kind;
-//     res.u.op1 = op1;
-//     res.u.op2 = op2;
-//     res.u.op3 = op3;
-//     res.u.size = size;
-//     strcpy(res.u.rel, rel);
-//     return res;
-// }
+void PrintList(InterCodeList* self, FILE *f){
+    InterCodeNode* cur = self->head_;
+    while(cur!=NULL){
+        Print_Code(cur->node_, f);
+        cur = cur->next;
+    }
+    return;
+}
--- intercode.h
+++ intercode.h
@@ -1,73 +1,78 @@
-// #ifndef INTERCODE
-// #define INTERCODE
+#ifndef INTERCODE
+#define INTERCODE
 
-// #include<stdio.h>
-// #include<stdlib.h>
-// #include<assert.h>
-// #include<string.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<assert.h>
+#include<string.h>
 
-// struct Operand_def{
-//     enum{
-//         OP_VARI,
-//         OP_ADD,
-//         OP_FUNC,
-//         OP_ARRAY,
-//         OP_LABEL,
-//         OP_CONST
-//     } kind;
+struct Operand_def{
+    enum{
+        OP_VARI,
+        OP_TEMP,
+        OP_LABEL,
+        OP_FUNC,
+        OP_CONST
+    } kind;
 
-//     union{
-//         int no_vari;
-//         int no_add;
-//         char name_func[20]; // not need to consider deep copy
-//         int no_array;
-//         int no_label;
-//         int val_const;
-//     } u;
-// };
-// typedef struct Operand_def Operand;
+    union{
+        int no_vari;
+        int no_label;
+        char *func_name;
+        int val_const;
+    } u;
+};
+typedef struct Operand_def Operand;
 
-// struct InterCode_def{
-//     enum{
-//         LABEL,
-//         FUNC,
-//         ASSIGN_VAL2VAL,
-//         ADD,
-//         SUB,
-//         MULTI,
-//         DIV,
-//         ASSIGN_ADD2VAL,
-//         ASSIGN_PNT2VAL,
-//         ASSIGN_VAL2PNT,
-//         GOTO,
-//         GOTO_COND,
-//         RETURN,
-//         DEC,
-//         ARG,
-//         CALL,
-//         PARAM,
-//         READ,
-//         WRITE
-//     } kind;
+struct InterCode_def{
+    enum{
+        LABEL,
+        FUNC,
+        ASSIGN_VAL2VAL,
+        ADD,
+        SUB,
+        MULTI,
+        DIV,
+        ASSIGN_ADD2VAL,
+        ASSIGN_PNT2VAL,
+        ASSIGN_VAL2PNT,
+        GOTO,
+        GOTO_COND,
+        RETURN,
+        DEC,
+        ARG,
+        CALL,
+        PARAM,
+        READ,
+        WRITE
+    } kind;
 
-//     struct{
-//         Operand op1, op2, op3;
-//         int size;
-//         char rel[3]; 
-//     } u;
-// };
-// typedef struct InterCode_def InterCode;
+    struct{
+        Operand op1, op2, op3;
+        char rel[3]; 
+    } u;
+};
+typedef struct InterCode_def InterCode;
 
-// struct InterCodeNode_def{
-//     InterCode node;
-//     struct InterCodeNode_def *prev, *next; 
-// };
-// typedef struct InterCodeNode_def InterCodeNode;
+void PrintCode(InterCode code, FILE* f);
+InterCode GenCode(int kind, Operand op1, Operand p2, Operand op3, char rel[]);
 
-// void Add_InterCode(InterCodeNode* head, InterCode code);
-// // void Print_OP(Operand* op, FILE* f);
-// void Print_Code(InterCode code, FILE* f);
-// void Print_List(InterCodeNode* head, FILE* f);
-// InterCode Gen_Code(int kind, Operand op1, Operand p2, Operand op3, int size, char *rel);
+////------------////
 
-// #endif
+struct InterCodeNode_def{
+    InterCode node_;
+    struct InterCodeNode_def *prev, *next; 
+};
+typedef struct InterCodeNode_def InterCodeNode;
+
+struct InterCodeList_def{
+    InterCodeNode* head_;
+    InterCodeNode* tail_;
+};
+typedef struct InterCodeList_def InterCodeList;
+
+InterCodeList* CodeListInit();
+void AddInterCode(InterCodeList* self, InterCode code);
+void PrintList(InterCodeList* self, FILE* f);
+
+#endif
